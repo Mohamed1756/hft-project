b8bcf297cdc8c60bc0f4f2404eb8e22d7634ac01
Mohamed <Mohamed.m365@outlook.com>
2025-02-01 13:16:50 +0000

    Round-robin implementation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
 use log::info;
-
-use hyperliquid_rust_sdk::{BaseUrl, InfoClient, Message, Subscription};
+use hyperliquid_rust_sdk::{BaseUrl, InfoClient, Message, Subscription, L2Book};
 use tokio::{
     spawn,
-    sync::mpsc::unbounded_channel,
+    sync::mpsc::{unbounded_channel, UnboundedSender},
     time::{sleep, Duration},
+    task::JoinHandle,
 };
+use std::time::Instant;
+
+// Number of parallel workers
+const WORKER_COUNT: usize = 4;
+
+struct Worker {
+    sender: UnboundedSender<L2Book>,
+    handle: JoinHandle<()>,
+}
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    let mut info_client = InfoClient::new(None, Some(BaseUrl::Testnet)).await.unwrap();
+    // Create worker pool
+    let workers: Vec<Worker> = (0..WORKER_COUNT)
+        .map(|id| {
+            let (tx, mut rx) = unbounded_channel();
+            let handle = spawn(async move {
+                while let Some(l2_book) = rx.recv().await {
+                    let start = Instant::now();
+                    process_update(&l2_book);
+                    println!("Worker {}: {:?}", id, start.elapsed());
+                }
+            });
+            Worker {
+                sender: tx,
+                handle,
+            }
+        })
+        .collect();
 
+    let mut info_client = InfoClient::new(None, Some(BaseUrl::Testnet)).await.unwrap();
     let (sender, mut receiver) = unbounded_channel();
     let subscription_id = info_client
         .subscribe(
@@ -30,8 +56,23 @@ async fn main() {
         info_client.unsubscribe(subscription_id).await.unwrap()
     });
 
-    // This loop ends when we unsubscribe
+    let mut worker_index = 0;
+    let start_time = Instant::now();
+
     while let Some(Message::L2Book(l2_book)) = receiver.recv().await {
-        info!("Received l2 book data: {l2_book:?}");
+        // Distribute work round-robin to workers
+        workers[worker_index].sender.send(l2_book).unwrap();
+        worker_index = (worker_index + 1) % WORKER_COUNT;
     }
+
+    println!("Total run time: {:?}", start_time.elapsed());
+
+    // Wait for all workers to complete
+    for worker in workers {
+        worker.handle.await.unwrap();
+    }
+}
+
+fn process_update(l2_book: &L2Book) {
+    info!("Processed order book update: {:?}", l2_book);
 }
